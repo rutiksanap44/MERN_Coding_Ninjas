a812f31a132a0387c55603e81905c37048ede001
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-04 14:04:33 +0530

    URL printing enabled

--- Node Js Workspace/First Server Setup Demo/serverFirst.js
+++ Node Js Workspace/First Server Setup Demo/serverFirst.js
@@ -1,8 +1,16 @@
 const http = require('http');
 const port = 2023;
 
-const server = http.createServer();
+// this function is used to print the url here by passing as parameter in http.createServer() function;
+// request and response
+function requestHandler(req, res){
+    console.log(req.url)
+}
 
+// to create server
+const server = http.createServer(requestHandler);
+
+// server that listens
 server.listen(port,function(err){
     if(err){
         console.log(err);
