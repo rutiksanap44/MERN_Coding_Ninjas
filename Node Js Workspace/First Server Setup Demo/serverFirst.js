05802700a3c78336de64138be25ffae37305841a
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-06 00:13:25 +0530

    Successfully sent the HTML as a Respose with fs module

--- Node Js Workspace/First Server Setup Demo/serverFirst.js
+++ Node Js Workspace/First Server Setup Demo/serverFirst.js
@@ -1,10 +1,27 @@
 const http = require('http');
 const port = 2023;
+const fs = require('fs');
 
 // this function is used to print the url here by passing as parameter in http.createServer() function;
-// request and response
 function requestHandler(req, res){
-    console.log(req.url)
+    console.log(req.url);
+    
+    res.writeHead(200, {'Content-type': 'text-html'});
+
+    // to send the response to the webpage
+    // this is not an ideal or appropriate way to write response, 
+    // therefor we will create seperate html file and use it here
+    // res.write('<h1>Response Send Successfully !<h1>');
+
+    // this is seperate html file reading way
+    // err is error and data is data we are getting from index.html
+    fs.readFile('./index.html',function(err,data){
+        if(err){
+            console.log(`Error Occured ${err}`);
+            return res.end('<h1>Error Ocurred !<h1>')
+        }
+        return res.end(data);
+    });
 }
 
 // to create server
